83376e0bc48703948e26ba4e2e7eceb70c78a616
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-11-08 15:52:34 +0000

    refactor: simplify test runner

--- test/runner.c
+++ test/runner.c
@@ -7,17 +7,10 @@
 #define EXIT_FAILURE -1
 
 int main(){
-    SRunner *sr = srunner_create(suite_create(""));
-
     // creates and adds the module test suites to the suite runner
-    Suite* suites[] = {
-        test_set_suite(),
-        test_vector_suite()
-    };
-    int suites_len = sizeof(suites) / sizeof(suites[0]);
-    for (int i = 0; i < suites_len; i++) {
-        srunner_add_suite(sr, suites[i]);
-    }
+    SRunner *sr = srunner_create(suite_create(""));
+    srunner_add_suite(sr, test_set_suite());
+    srunner_add_suite(sr, test_vector_suite());
 
     // runs all test suites and checks for failures
     srunner_run_all(sr, CK_VERBOSE);
