6cb529dec39bc6c80c73a5d7c89b0a79ffd6e6c0
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-11-11 16:41:10 +0000

    fix: avoid redefinition of constants from other libs

--- test/runner.c
+++ test/runner.c
@@ -3,8 +3,8 @@
 #include "test_vector.c"
 
 #define SUITE_NAME "libdsa"
-#define EXIT_SUCCESS 0
-#define EXIT_FAILURE -1
+#define SUITE_SUCCESS 0
+#define SUITE_FAILURE -1
 
 int main() {
     // creates and adds the module test suites to the suite runner
@@ -15,5 +15,5 @@ int main() {
     srunner_run_all(sr, CK_VERBOSE);
     int failed = srunner_ntests_failed(sr);
     srunner_free(sr);
-    return failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+    return failed == 0 ? SUITE_SUCCESS : SUITE_FAILURE;
 }
