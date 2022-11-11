9d3424dfd0a796b7701a17c73a107cd590a19b9f
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-11-11 16:43:01 +0000

    test: rename test

--- test/runner.c
+++ test/runner.c
@@ -9,7 +9,7 @@
 int main() {
     // creates and adds the module test suites to the suite runner
     SRunner *sr = srunner_create(suite_create(SUITE_NAME));
-    srunner_add_suite(sr, test_vector_suite());
+    srunner_add_suite(sr, suite_vector());
 
     // runs all test suites and checks for failures
     srunner_run_all(sr, CK_VERBOSE);
--- test/test_vector.c
+++ test/test_vector.c
@@ -4,13 +4,12 @@
 
 START_TEST(test_vector) {
     Vector *vec = vector(sizeof(int), 10);
-    ck_assert_ptr_nonnull(vec);
     ck_assert_uint_eq(vector_length(vec), 0);
     ck_assert_uint_eq(vector_capacity(vec), 10);
 }
 END_TEST
 
-Suite* test_vector_suite() {
+Suite* suite_vector() {
     Suite *suite = suite_create("vector");
     TCase *test_case = tcase_create("");
     tcase_add_test(test_case, test_vector);
