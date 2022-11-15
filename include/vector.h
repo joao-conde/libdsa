8fee50fb2cc71d54a6342644dd78b45dcf4619d4
João Conde <joaodiasconde@gmail.com>
2022-11-15 14:15:14 +0000

    fix: remaining vector variables

--- include/vector.h
+++ include/vector.h
@@ -12,27 +12,27 @@ vector* vec_with_capacity(size_t type_size, unsigned int capacity);
 
 vector* vec_from_array(size_t type_size, unsigned int length, const void *array);
 
-void vec_free(vector *vector);
+void vec_free(vector *v);
 
-unsigned int vec_length(const vector *vector);
+unsigned int vec_length(const vector *v);
 
-unsigned int vec_capacity(const vector *vector);
+unsigned int vec_capacity(const vector *v);
 
 bool vec_empty(const vector *v);
 
-void* vec_at(const vector *vector, unsigned int index);
+void* vec_at(const vector *v, unsigned int index);
 
 void* vec_begin(const vector *v);
 
 void* vec_end(const vector *v);
 
-void* vec_push(vector *vector, void *value);
+void* vec_push(vector *v, void *value);
 
-void* vec_pop(vector *vector);
+void* vec_pop(vector *v);
 
-void* vec_insert(vector *vector, unsigned int index, void *value);
+void* vec_insert(vector *v, unsigned int index, void *value);
 
-void* vec_erase(vector *vector, unsigned int index);
+void* vec_erase(vector *v, unsigned int index);
 
 void* vec_resize(vector *v, unsigned int capacity);
 
