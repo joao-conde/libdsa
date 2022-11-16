5f059df7072105d9c893215b425f04069e74d693
João Conde <joaodiasconde@gmail.com>
2022-11-16 12:15:39 +0000

    docs: code comments for vector

--- src/structures/vector.c
+++ src/structures/vector.c
@@ -46,6 +46,9 @@ vector* vec_with_capacity(size_t type_size, unsigned int capacity) {
 }
 
 vector* vec_from_array(size_t type_size, unsigned int length, const void *array) {
+    // computes vector capacity based on the array to copy length
+    // if the vector would be at capacity by copying the original
+    // array then we allocate a bigger one
     unsigned int capacity = CAPACITY > length ? CAPACITY : length * ALLOC_FACTOR;
 
     vector *v = malloc(sizeof(vector));
@@ -56,6 +59,7 @@ vector* vec_from_array(size_t type_size, unsigned int length, const void *array)
         return NULL;
     }
 
+    // copies the array to copy contents to internal data buffer
     memcpy(data, array, length * type_size);
 
     v->length = length;
@@ -96,45 +100,85 @@ void* vec_at(const vector *v, unsigned int index) {
 }
 
 void* vec_push(vector *v, const void *value) {
+    // if the vector is at capacity already we resize it
+    // if the resizing fails the push operation halts
     if (v->length >= v->capacity) {
-        void *result = vec_resize(v, v->capacity * ALLOC_FACTOR);
-        if (result == NULL) return NULL;
+        unsigned int capacity = v->capacity > 0 ? v->capacity * ALLOC_FACTOR : CAPACITY;
+        void *resized = vec_resize(v, capacity);
+        if (resized == NULL) return NULL;
     }
 
     uint8_t *dest = (uint8_t*) v->data + v->length * v->type_size;
     v->length += 1;
-    return memcpy(dest, value, v->type_size);
+
+    // returns a pointer to the pushed value
+    void *pushed = memcpy(dest, value, v->type_size);
+    return pushed;
 }
 
 void* vec_pop(vector *v) {
     if (v->length == 0) return NULL;
-    void *popped = (uint8_t*) v->data + (v->length - 1) * v->type_size;
+
     v->length -= 1;
+
+    // returns a pointer to the popped element
+    void *popped = (uint8_t*) v->data + v->length * v->type_size;
     return popped;
 }
 
 void* vec_insert(vector *v, unsigned int index, const void *value) {
     if (index > v->length) return NULL;
+
+    // if the vector is at capacity already we resize it
+    // if the resizing fails the insert operation halts
     if (v->length >= v->capacity) {
-        void *result = vec_resize(v, v->capacity * ALLOC_FACTOR);
-        if (result == NULL) return NULL;
+        unsigned int capacity = v->capacity > 0 ? v->capacity * ALLOC_FACTOR : CAPACITY;
+        void *resized = vec_resize(v, capacity);
+        if (resized == NULL) return NULL;
     }
-    void *pos = (uint8_t*) v->data + index * v->type_size;
+
+    // computes the number of elements to copy and the
+    // position of insertion
     unsigned int to_copy = v->length - index;
-    memmove(pos + v->type_size, pos, to_copy * v->type_size);
+    void *pos = (uint8_t*) v->data + index * v->type_size;
+
+    // shifts all elements right from the insertion position forward
+    // and sets the insertion position to the inserted value
+    // halts the insert operation if this shift fails
+    void *moved = memmove(pos + v->type_size, pos, to_copy * v->type_size);
+    if (moved == NULL) return NULL;
+
     v->length += 1;
-    return memcpy(pos, value, v->type_size);
+
+    // returns a pointer to the inserted value
+    void *inserted = memcpy(pos, value, v->type_size);
+    return inserted;
 }
 
 void* vec_erase(vector *v, unsigned int index) {
     if (index > v->length) return NULL;
-    void *pos = (uint8_t*) v->data + index * v->type_size;
+
+    // computes the number of elements to copy and the
+    // position of deletion
     unsigned int to_copy = v->length - index;
+    void *pos = (uint8_t*) v->data + index * v->type_size;
+
+    // shifts all elements left from the deletion position forward
+    // halts the insert operation if this shift fails
+    void *moved = memmove(pos, pos + v->type_size, to_copy * v->type_size);
+    if (moved == NULL) return NULL;
+
     v->length -= 1;
-    return memmove(pos, pos + v->type_size, to_copy * v->type_size);
+
+    // returns a pointer to the position of the erased
+    // element where a new vector element resides
+    return moved;
 }
 
 void* vec_resize(vector *v, unsigned int capacity) {
+    // attempts to resize the internal data buffer
+    // failure is detected if a NULL pointer is returned
+    // and the resizing was not of zero bytes
     void *data = realloc(v->data, v->type_size * capacity);
     if (capacity != 0 && data == NULL) return NULL;
 
