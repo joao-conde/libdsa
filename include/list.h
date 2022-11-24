97ebd901868458ec391bf89b469bb94d56556d7e
João Conde <joaodiasconde@gmail.com>
2022-11-24 15:05:18 +0000

    feat: doubly linked list interface

--- include/list.h
+++ include/list.h
@@ -1,6 +1,35 @@
 #ifndef INCLUDE_LIST_H_
 #define INCLUDE_LIST_H_
 
+#include <stdbool.h>
+#include <stdlib.h>
+
 typedef struct list list;
+
+list* list_init(size_t type_size);
+
+void list_free(list *l);
+
+size_t list_length(const list *l);
+
+bool list_is_empty(const list *l);
+
+void list_clear(list *l);
+
+void* list_front(const list *l);
+
+void* list_back(const list *l);
+
+void* list_push_back(list *l, const void *value);
+
+void* list_push_front(list *l, const void *value);
+
+void* list_pop_back(list *l);
+
+void* list_pop_front(list *l);
+
+void* list_insert(list *l, size_t index, const void *value);
+
+void* list_erase(list *l, size_t index);
 
 #endif // INCLUDE_LIST_H_
--- src/list.c
+++ src/list.c
@@ -1,1 +1,16 @@
 #include "../include/list.h"
+
+struct list {
+    size_t length;
+};
+
+list* list_init(size_t type_size) {
+    list *l = malloc(sizeof(list));
+    if (l == NULL) return NULL;
+    l->length = 0;
+    return l;
+}
+
+void list_free(list *l) {
+    free(l);
+}
