a6386a451e10a435f3b8335f895ebe05ab4d2786
João Conde <joaodiasconde@gmail.com>
2022-11-25 13:11:02 +0000

    test: more list tests

--- test/list.c
+++ test/list.c
@@ -132,6 +132,140 @@ START_TEST(test_list_pointers) {
 }
 END_TEST
 
+START_TEST(test_list_push_back) {
+    int *result, values[5] = {51, 12, -123, 1000, -1};
+    list *l = list_init(sizeof(int));
+
+    result = list_push_back(l, &values[0]);
+    ck_assert(*result == 51);
+
+    result = list_push_back(l, &values[1]);
+    ck_assert(*result == 12);
+
+    result = list_push_back(l, &values[2]);
+    ck_assert(*result == -123);
+
+    result = list_push_back(l, &values[3]);
+    ck_assert(*result == 1000);
+
+    result = list_push_back(l, &values[4]);
+    ck_assert(*result == -1);
+
+    ck_assert(list_length(l) == 5);
+
+    list_free(l);
+}
+END_TEST
+
+START_TEST(test_list_push_front) {
+    int *result, values[5] = {51, 12, -123, 1000, -1};
+    list *l = list_init(sizeof(int));
+
+    result = list_push_front(l, &values[0]);
+    ck_assert(*result == 51);
+
+    result = list_push_front(l, &values[1]);
+    ck_assert(*result == 12);
+
+    result = list_push_front(l, &values[2]);
+    ck_assert(*result == -123);
+
+    result = list_push_front(l, &values[3]);
+    ck_assert(*result == 1000);
+
+    result = list_push_front(l, &values[4]);
+    ck_assert(*result == -1);
+
+    ck_assert(list_length(l) == 5);
+
+    list_free(l);
+}
+END_TEST
+
+START_TEST(test_list_pop_back) {
+    int *result, values[5] = {51, 12, -123, 1000, -1};
+    list *l = list_init(sizeof(int));
+    list_push_back(l, &values[0]);
+    list_push_back(l, &values[1]);
+    list_push_back(l, &values[2]);
+    list_push_back(l, &values[3]);
+    list_push_back(l, &values[4]);
+
+    result = list_back(l);
+    ck_assert(*result == -1);
+    ck_assert(list_length(l) == 5);
+
+    list_pop_back(l);
+    result = list_back(l);
+    ck_assert(*result == 1000);
+    ck_assert(list_length(l) == 4);
+
+    list_pop_back(l);
+    result = list_back(l);
+    ck_assert(*result == -123);
+    ck_assert(list_length(l) == 3);
+
+    list_pop_back(l);
+    result = list_back(l);
+    ck_assert(*result == 12);
+    ck_assert(list_length(l) == 2);
+
+    list_pop_back(l);
+    result = list_back(l);
+    ck_assert(*result == 51);
+    ck_assert(list_length(l) == 1);
+
+    list_pop_back(l);
+    result = list_back(l);
+    ck_assert(result == NULL);
+    ck_assert(list_length(l) == 0);
+
+    list_free(l);
+}
+END_TEST
+
+START_TEST(test_list_pop_front) {
+    int *result, values[5] = {51, 12, -123, 1000, -1};
+    list *l = list_init(sizeof(int));
+    list_push_front(l, &values[0]);
+    list_push_front(l, &values[1]);
+    list_push_front(l, &values[2]);
+    list_push_front(l, &values[3]);
+    list_push_front(l, &values[4]);
+
+    result = list_front(l);
+    ck_assert(*result == -1);
+    ck_assert(list_length(l) == 5);
+
+    list_pop_front(l);
+    result = list_front(l);
+    ck_assert(*result == 1000);
+    ck_assert(list_length(l) == 4);
+
+    list_pop_front(l);
+    result = list_front(l);
+    ck_assert(*result == -123);
+    ck_assert(list_length(l) == 3);
+
+    list_pop_front(l);
+    result = list_front(l);
+    ck_assert(*result == 12);
+    ck_assert(list_length(l) == 2);
+
+    list_pop_front(l);
+    result = list_front(l);
+    ck_assert(*result == 51);
+    ck_assert(list_length(l) == 1);
+
+    list_pop_front(l);
+    result = list_front(l);
+    ck_assert(result == NULL);
+    ck_assert(list_length(l) == 0);
+
+    list_free(l);
+}
+END_TEST
+
 Suite* suite_list() {
     Suite *suite = suite_create("list");
     TCase *test_case = tcase_create("");
@@ -142,6 +276,10 @@ Suite* suite_list() {
     tcase_add_test(test_case, test_list_front);
     tcase_add_test(test_case, test_list_back);
     tcase_add_test(test_case, test_list_pointers);
+    tcase_add_test(test_case, test_list_push_back);
+    tcase_add_test(test_case, test_list_push_front);
+    tcase_add_test(test_case, test_list_pop_back);
+    tcase_add_test(test_case, test_list_pop_front);
     suite_add_tcase(suite, test_case);
     return suite;
 }
