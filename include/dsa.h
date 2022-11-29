31d4dbdbe397a5755d554d61fa424b094e1cbf78
João Conde <joaodiasconde@gmail.com>
2022-11-29 13:32:12 +0000

    version: 0.5.0

--- include/dsa.h
+++ include/dsa.h
@@ -1,9 +1,10 @@
 #ifndef INCLUDE_DSA_H_
 #define INCLUDE_DSA_H_
 
-#define DSA_VERSION "0.4.0"
+#define DSA_VERSION "0.5.0"
 
 #include "deque.h"
+#include "list.h"
 #include "queue.h"
 #include "stack.h"
 #include "vector.h"
