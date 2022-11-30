ae879b660032a0644450dee22cb4d3a45508f1ba
joao-conde <joaodiasconde@gmail.com>
2022-11-30 15:52:25 +0000

    test: better list stress test

--- test/list.c
+++ test/list.c
@@ -411,36 +411,106 @@ START_TEST(test_list_stress) {
     int nelements = 100000;
     bool is_empty;
     size_t length;
-    int *pushed, *front, *back;
+    node *pushed, *inserted, *erased, *front, *back, *current;
 
     list *l = list_init(sizeof(int));
 
     for (int i = 0; i < nelements; i++) {
-        pushed = list_push_back(l, &i)->data;
+        pushed = list_push_back(l, &i);
         length = list_length(l);
         is_empty = list_is_empty(l);
-        front = list_front(l)->data;
-        back = list_back(l)->data;
-        ck_assert(*pushed == i);
+        front = list_front(l);
+        back = list_back(l);
+        ck_assert(*(int*)pushed->data == i);
         ck_assert(length == i + 1);
         ck_assert(!is_empty);
-        ck_assert(*front == 0);
-        ck_assert(*back == i);
+        ck_assert(*(int*)front->data == 0);
+        ck_assert(*(int*)back->data == i);
     }
 
     for (int i = 0; i < nelements - 1; i++) {
         list_pop_back(l);
         length = list_length(l);
         is_empty = list_is_empty(l);
-        front = list_front(l)->data;
-        back = list_back(l)->data;
+        front = list_front(l);
+        back = list_back(l);
         ck_assert(length == nelements - i - 1);
         ck_assert(!is_empty);
-        ck_assert(*front == 0);
-        ck_assert(*back == nelements - i - 2);
+        ck_assert(*(int*)front->data == 0);
+        ck_assert(*(int*)back->data == nelements - i - 2);
     }
 
     list_pop_back(l);
+    length = list_length(l);
+    is_empty = list_is_empty(l);
+    ck_assert(length == 0);
+    ck_assert(is_empty);
+    ck_assert(list_front(l) == NULL);
+    ck_assert(list_back(l) == NULL);
+
+    for (int i = 0; i < nelements; i++) {
+        pushed = list_push_front(l, &i);
+        length = list_length(l);
+        is_empty = list_is_empty(l);
+        front = list_front(l);
+        back = list_back(l);
+        ck_assert(*(int*)pushed->data == i);
+        ck_assert(length == i + 1);
+        ck_assert(!is_empty);
+        ck_assert(*(int*)front->data == i);
+        ck_assert(*(int*)back->data == 0);
+    }
+
+    for (int i = 0; i < nelements - 1; i++) {
+        list_pop_front(l);
+        length = list_length(l);
+        is_empty = list_is_empty(l);
+        front = list_front(l);
+        back = list_back(l);
+        ck_assert(length == nelements - i - 1);
+        ck_assert(!is_empty);
+        ck_assert(*(int*)front->data == nelements - i - 2);
+        ck_assert(*(int*)back->data == 0);
+    }
+
+    list_pop_front(l);
+    length = list_length(l);
+    is_empty = list_is_empty(l);
+    ck_assert(length == 0);
+    ck_assert(is_empty);
+    ck_assert(list_front(l) == NULL);
+    ck_assert(list_back(l) == NULL);
+
+    current = list_push_front(l, &pushed);
+    for (int i = 0; i < nelements - 1; i++) {
+        inserted = list_insert(l, current, &i);
+        length = list_length(l);
+        is_empty = list_is_empty(l);
+        front = list_front(l);
+        back = list_back(l);
+        ck_assert(*(int*)inserted->data == i);
+        ck_assert(length == i + 2);
+        ck_assert(!is_empty);
+        ck_assert(front == current);
+        ck_assert(*(int*)back->data == 0);
+    }
+
+    current = list_front(l);
+    for (int i = 0; i < nelements - 1; i++) {
+        erased = current;
+        current = current->next != NULL ? current->next : current->prev;
+        list_erase(l, erased);
+        length = list_length(l);
+        is_empty = list_is_empty(l);
+        front = list_front(l);
+        back = list_back(l);
+        ck_assert(length == nelements - i - 1);
+        ck_assert(!is_empty);
+        ck_assert(front != NULL);
+        ck_assert(back != NULL);
+    }
+
+    list_pop_front(l);
     length = list_length(l);
     is_empty = list_is_empty(l);
     ck_assert(length == 0);
