a52da62c945669d58a1fffb3d083586b14359982
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2023-03-17 19:48:48 +0000

    feat: pair setters for elements

--- include/pair.h
+++ include/pair.h
@@ -20,4 +20,8 @@ void* pair_first(const pair *p);
 
 void* pair_second(const pair *p);
 
+void pair_set_first(pair *p, const void* first);
+
+void pair_set_second(pair *p, const void* second);
+
 #endif // INCLUDE_PAIR_H_
