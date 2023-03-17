f60e9c570aab3cfee0db9a365e002e26aa7bd924
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2023-03-17 20:05:00 +0000

    bench: vector pop

--- benchmark/vector.c
+++ benchmark/vector.c
@@ -7,3 +7,14 @@ void bench_c_vector_push() {
     }
     vector_free(v);
 }
+
+void bench_c_vector_pop() {
+    vector *v = vector_init(sizeof(int));
+    for (size_t i = 0; i < ITER_VECTOR_PUSH; i++) {
+        vector_push(v, &i);
+    }
+    for (size_t i = 0; i < ITER_VECTOR_POP; i++) {
+        vector_pop(v);
+    }
+    vector_free(v);
+}
