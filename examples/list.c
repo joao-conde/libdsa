6a6ab423cbb909c13b2904ac5ee85f1d8af6c89a
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2023-03-20 12:48:57 +0000

    example: list

---
+++ examples/list.c
@@ -0,0 +1,24 @@
+#include <dsa.h>
+#include <list.h>
+
+#include "mytype.h"
+
+extern mytype m1, m2, m3;
+
+int main() {
+    list *l = list_init(sizeof(mytype));
+
+    list_push_back(l, &m1);
+    list_push_front(l, &m2);
+
+    node *front = list_front(l);
+    node *back = list_back(l);
+
+    list_insert(l, front, &m3);
+    list_erase(l, front);
+
+    list_pop_front(l);
+    list_pop_back(l);
+
+    list_free(l);
+}
