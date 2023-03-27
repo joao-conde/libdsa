9ed36d6a863608c9446dc2ed3f6e6092af6dbd39
João Conde <joaodiasconde@gmail.com>
2023-03-27 11:09:02 +0100

    feat: map find, get, insert

--- test/map.c
+++ test/map.c
@@ -2,15 +2,19 @@
 
 #include "../include/map.h"
 
+size_t hash_size_t(const void *key) {
+    return *(size_t*)(key);
+}
+
 void test_map_init() {
-    map *m = map_init(sizeof(int), sizeof(int));
+    map *m = map_init(sizeof(size_t), sizeof(size_t), hash_size_t);
     map_free(m);
 }
 
 void test_map_free() {
     map_free(NULL);
 
-    map *m = map_init(sizeof(int), sizeof(int));
+    map *m = map_init(sizeof(size_t), sizeof(size_t), hash_size_t);
     map_free(m);
 }
 
