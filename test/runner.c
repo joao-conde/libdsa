7b24c3b50fb200686948e2b5bc24f7c982636dc7
João Conde <joaodiasconde@gmail.com>
2023-03-27 17:30:18 +0100

    test: run map tests

--- test/runner.c
+++ test/runner.c
@@ -1,5 +1,6 @@
 #include "./deque.c"
 #include "./list.c"
+#include "./map.c"
 #include "./pair.c"
 #include "./queue.c"
 #include "./stack.c"
@@ -8,6 +9,7 @@
 int main() {
     test_deque();
     test_list();
+    test_map();
     test_pair();
     test_queue();
     test_stack();
