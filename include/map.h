1dd875e6beec5d263b025efa210fa5e45c640fd5
João Conde <joaodiasconde@gmail.com>
2023-03-27 12:11:41 +0100

    feat: map erase and syntatic sugar

--- include/map.h
+++ include/map.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_MAP_H_
 #define INCLUDE_MAP_H_
 
+#include <stdbool.h>
 #include <stdlib.h>
 
 #include "../include/pair.h"
@@ -15,10 +16,16 @@ void map_free(map *m);
 
 size_t map_length(const map *m);
 
+bool map_is_empty(const map *m);
+
+bool map_has(const map *m, const void *key);
+
 pair* map_find(const map *m, const void *key);
 
 void* map_get(const map *m, const void *key);
 
 pair* map_insert(map *m, const void *key, const void *value);
+
+void map_erase(map *m, const void *key);
 
 #endif // INCLUDE_MAP_H_
