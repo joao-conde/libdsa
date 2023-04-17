8c48055dc2ca303a002c6a97ddb4b106c7af3421
João Conde <joaodiasconde@gmail.com>
2023-04-17 15:45:37 +0100

    feat: dumb set implementation

---
+++ src/internal/set.h
@@ -0,0 +1,6 @@
+#ifndef SRC_INTERNAL_SET_H_
+#define SRC_INTERNAL_SET_H_
+
+#define NBUCKETS 512
+
+#endif // SRC_INTERNAL_SET_H_
--- src/set.c
+++ src/set.c
@@ -1,31 +1,74 @@
+#include "internal/set.h"
+
+#include "../include/map.h"
 #include "../include/set.h"
 
-struct set {};
+struct set {
+    map *data;
+};
 
-set* set_init(size_t type_size, hash_fn *hasher) {}
+set* set_init(size_t type_size, hash_fn *hasher) {
+    return set_with_buckets(type_size, hasher, NBUCKETS);
+}
 
-set* set_with_buckets(size_t type_size, hash_fn *hasher, size_t nbuckets) {}
+set* set_with_buckets(size_t type_size, hash_fn *hasher, size_t nbuckets) {
+    set *s = (set*) malloc(sizeof(set));
+    map *data = map_with_buckets(type_size, type_size, hasher, nbuckets);
+    if (s == NULL || data == NULL) {
+        map_free(data);
+        free(s);
+        return NULL;
+    }
 
-void set_free(set *s) {}
+    s->data = data;
+    return s;
+}
 
-float set_max_load_factor(const set *s) {}
+void set_free(set *s) {
+    if (s != NULL) map_free(s->data);
+    free(s);
+}
 
-void set_set_max_load_factor(set *s, float factor) {}
+float set_max_load_factor(const set *s) {
+    return map_max_load_factor(s->data);
+}
 
-float set_load_factor(const set *s) {}
+void set_set_max_load_factor(set *s, float factor) {
+    map_set_max_load_factor(s->data, factor);
+}
 
-size_t set_size(const set *s) {}
+float set_load_factor(const set *s) {
+    return map_load_factor(s->data);
+}
 
-size_t set_buckets(const set *s) {}
+size_t set_size(const set *s) {
+    return map_size(s->data);
+}
 
-bool set_empty(const set *s) {}
+size_t set_buckets(const set *s) {
+    return map_buckets(s->data);
+}
 
-void set_clear(set *s) {}
+bool set_empty(const set *s) {
+    return map_empty(s->data);
+}
 
-bool set_has(const set *s, const void *value) {}
+void set_clear(set *s) {
+    map_clear(s->data);
+}
 
-void* set_insert(set *s, const void *value) {}
+bool set_has(const set *s, const void *value) {
+    return map_has(s->data, value);
+}
 
-void set_erase(set *s, const void *value) {}
+void* set_insert(set *s, const void *value) {
+    return map_insert(s->data, value, value);
+}
 
-void set_rehash(set *s, size_t nbuckets) {}
+void set_erase(set *s, const void *value) {
+    map_erase(s->data, value);
+}
+
+void set_rehash(set *s, size_t nbuckets) {
+    map_rehash(s->data, nbuckets);
+}
