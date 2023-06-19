2623a6126cc14f7a7007f27448fd9e5748ec488d
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2023-06-19 07:55:41 +0100

    feat: heap examples

---
+++ examples/heap.c
@@ -0,0 +1,25 @@
+#include <dsa.h>
+
+#include "mytype.h"
+
+extern mytype m1, m2, m3;
+
+bool lesser(const void *a, const void *b) {
+    return ((mytype*) a)->id < ((mytype*) b)->id;
+}
+
+int main() {
+    heap *h = heap_init(sizeof(mytype), lesser);
+
+    heap_push(h, &m1);
+    heap_push(h, &m2);
+    heap_push(h, &m3);
+
+    mytype *m = (mytype*) heap_max(h);
+
+    heap_pop(h);
+
+    heap_clear(h);
+
+    heap_free(h);
+}
